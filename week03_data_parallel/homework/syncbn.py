73f0642f9d75555ef47e6e40a45c98bf10c6eb3f
Lavr Pliskovsky <l0ver1ck.study@gmail.com>
2026-02-16 03:01:54 +0500

    week03: task 2 part 1

--- week03_data_parallel/homework/syncbn.py
+++ week03_data_parallel/homework/syncbn.py
@@ -17,15 +17,77 @@ class sync_batch_norm(Function):
     """
 
     @staticmethod
-    def forward(ctx, input, running_mean, running_std, eps: float, momentum: float):
+    def forward(ctx, input, running_mean, running_std, eps: float, momentum: float, training: bool):
         # Compute statistics, sync statistics, apply them to the input
         # Also, store relevant quantities to be used on the backward pass with `ctx.save_for_backward`
-        pass
+
+        n, c, l = input.shape
+        cnt = n * l
+
+        if training:
+            local_sum = input.sum(axis=(0, 2))
+            local_sum_sq = (input ** 2).sum(axis=(0, 2))
+            local_count = torch.tensor([cnt], device=input.device, dtype=input.dtype)
+
+            stats = torch.cat([local_sum, local_sum_sq, local_count], dim=0)
+            if dist.is_available() and dist.is_initialized():
+                dist.all_reduce(stats)
+
+            global_sum = stats[:c]
+            global_sum_sq = stats[c : 2 * c]
+            global_count = stats[-1]
+
+            mean = global_sum / global_count
+            var = global_sum_sq / global_count - mean * mean
+
+            if global_count > 1:
+                unbiased_var = var * global_count / (global_count - 1)
+            else:
+                unbiased_var = var
+
+            running_mean.mul_(1 - momentum).add_(mean, alpha=momentum)
+            running_std.mul_(1 - momentum).add_(unbiased_var, alpha=momentum)
+            count_for_backward = local_count
+        else:
+            mean = running_mean
+            var = running_std
+            count_for_backward = torch.tensor([cnt], device=input.device, dtype=input.dtype)
+
+        forward_centered_inputs = input - mean[None, :, None]
+        forward_inverse_std = 1 / torch.sqrt(eps + var[None, :, None])
+        forward_normalized_inputs = forward_centered_inputs * forward_inverse_std
+
+        ctx.save_for_backward(forward_normalized_inputs, forward_inverse_std, count_for_backward)
+        ctx.training = training
+        
+        return forward_normalized_inputs
 
     @staticmethod
     def backward(ctx, grad_output):
         # don't forget to return a tuple of gradients wrt all arguments of `forward`!
-        pass
+        forward_normalized_inputs, forward_inverse_std, local_count = ctx.saved_tensors
+
+        if not ctx.training:
+            grad_input = grad_output * forward_inverse_std
+            return grad_input, *(None,) * 5
+
+        c = grad_output.shape[1]
+        local_grad_sum = grad_output.sum(axis=(0, 2))
+        local_grad_norm_sum = (grad_output * forward_normalized_inputs).sum(axis=(0, 2))
+        stats = torch.cat([local_grad_sum, local_grad_norm_sum, local_count], dim=0)
+        if dist.is_available() and dist.is_initialized():
+            dist.all_reduce(stats)
+
+        grad_mean = stats[:c] / stats[-1]
+        grad_norm_mean = stats[c : 2 * c] / stats[-1]
+
+        grad_input = forward_inverse_std * (
+            grad_output
+            - grad_mean[None, :, None]
+            - forward_normalized_inputs * grad_norm_mean[None, :, None]
+        )
+
+        return grad_input, *(None,) * 5
 
 
 class SyncBatchNorm(_BatchNorm):
@@ -45,9 +107,14 @@ class SyncBatchNorm(_BatchNorm):
             dtype=None,
         )
         # your code here
-        self.running_mean = torch.zeros((num_features,))
-        self.running_std = torch.ones((num_features,))
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         # You will probably need to use `sync_batch_norm` from above
-        pass
+        return sync_batch_norm.apply(
+            input, 
+            self.running_mean, 
+            self.running_var, 
+            self.eps, 
+            self.momentum,
+            self.training,
+        )
